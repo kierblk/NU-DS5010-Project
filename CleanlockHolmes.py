8035f785c0f3ce7acf90b713704c35240ed4a820
sezzagore91 <127037809+sezzagore91@users.noreply.github.com>
2023-04-03 09:32:57 -0400

    Update 04/03
    Updated version of the package as of 9:31am 04/03

--- CleanlockHolmes.py
+++ CleanlockHolmes.py
@@ -18,11 +18,11 @@ class CleanlockHolmes:
     
 
     def read_data(self, input_file):
-    """
-    Function to read data from an input file and stores it in a pandas DataFrame
-    param input_file: input file from user
-    returns None
-    """
+        """
+        Function to read data from an input file and stores it in a pandas DataFrame
+        param input_file: input file from user
+        returns None
+        """
         file_name, file_format = input_file.split(".")
 
         if file_format == 'csv':
@@ -49,7 +49,11 @@ class CleanlockHolmes:
         returns None
 
         """
-        pass
+    
+        self.invalid_dictionary[col_name] = invalid_values_list
+
+        return self.invalid_dictionary
+            
 
     def specify_valid_entries(self,valid_values_list, col_name):
         """
@@ -62,17 +66,42 @@ class CleanlockHolmes:
 
         """
 
-        pass
+        self.valid_dictionary[col_name] = valid_values_list
 
-    def clean_data(self, method, arg = {}):
+        return self.valid_dictionary
+
+    def identify_invalid_values(self):
 
         """
-        Function utalizes chosen method to rectify problematic value entries.
+        identifies row/col pairs that contain an invalid value 
+        param None
+        returns a list of indicies of corresponding to invalid data entries
+        """
+        rows , columns = self.data_object.shape
+        invalid_values_tracker = np.zeros((rows - 1 , columns))
+        print(invalid_values_tracker)
+
+        for j in range(len(self.columns)):
+            col = self.columns[j]
+            for i in range(rows -1):
+                if self.invalid_dictionary.get(col):
+                    if self.data_object.loc[i].at[col] in self.invalid_dictionary[col]:
+                        invalid_values_tracker[i][j] = 1
+                elif self.valid_dictionary.get(col):
+                    if self.data_object.loc[i].at[col] not in self.valid_dictionary[col]:
+                        invalid_values_tracker[i][j] = 1
+                    
+        return invalid_values_tracker
+                
+
+    def clean_data(self, method, invalid_values_tracker, arg = {}):
+
+        """
+        Function utilizes chosen method to rectify problematic value entries.
         Selects a method from these options:
         method 1 drop_row: removes data point
         method 2 replace_row : replaces problematic data point with values
         specified in arg["replacement"]
-        method 3 interpolation : (TO DO)
         param
 
                 method - rectification method specified by user
@@ -80,8 +109,19 @@ class CleanlockHolmes:
         returns None
 
         """
+        rows, columns = invalid_values_tracker.shape
+        counter = 0
+        if method == 1:
+            for i in range(rows):
+                total = sum(invalid_values_tracker[i])
+                if total > 0:
+                    self.data_object.drop(labels = i - counter, axis = 0)
+                    print("dropped row")
+                    counter += 1
 
-        pass
+        
+          
+            
 
     def write_data(self, new_file_name):
 
@@ -90,4 +130,26 @@ class CleanlockHolmes:
         param new_file_name: specified file
         returns None
         """
-        
+
+        pass
+
+if __name__ == "__main__":
+
+    data_object = CleanlockHolmes("bmi_gender_corrupted.csv")
+
+    output = data_object.specify_invalid_entries(['NA', '', 'null', '--'], 'Weight')
+    print(output)
+
+    #data_object.specify_invalid_entries([“NA”, “”, “null”, “--”], “Height”)
+    #data_object.specify_invalid_entries([“NA”, “”, “null”, “--”, “0”], “Index”)
+
+
+    output2 = data_object.specify_valid_entries(['Male', 'Female', 'Not specified'] , 'Gender')
+    print(output2)
+
+    output_3 = data_object.identify_invalid_values()
+    
+    data_object.clean_data(1, output_3)
+
+    #data_object.write_data(new_file_name)
+

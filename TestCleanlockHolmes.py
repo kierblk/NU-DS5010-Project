4bb41a18ff8b2e71a8f634ef95fc44197818b869
sezzagore91 <127037809+sezzagore91@users.noreply.github.com>
2023-04-07 09:19:39 -0400

    Create TestCleanlockHolmes.py

---
+++ TestCleanlockHolmes.py
@@ -0,0 +1,53 @@
+import unittest
+from CleanlockHolmes import CleanlockHolmes
+
+class TestCleanlockHolmes(unittest.TestCase):
+
+# for the following tests --> self.assertEqual; self.assertTrue
+
+    def test_init(self):
+        '''
+        Tests if CleanLockHolmes properly initializes by checking inputs
+        '''
+        pass
+
+    def read_data(self):
+        '''
+        Tests if read_data returns 
+        '''
+        pass
+
+    def specify_invalid_entries(self):
+        '''
+        Tests if 
+        '''
+        pass
+
+    def specify_valid_entries(self):
+        '''
+        test
+        '''
+        pass
+
+    def identify_invalid_values(self):
+        '''
+        test
+        '''
+        pass
+
+    def clean_data(self):
+        '''
+        test
+        '''
+        pass
+
+    def write_data(self):
+        '''
+        test
+        '''
+        pass
+
+
+def main():
+    unittest.main(verbosity=3)
+main()
